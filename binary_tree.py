09cb26b6eb0e6ec4944a9fc9b4afb38164ed54cb
redfast00 <redfast00@gmail.com>
2018-02-28 16:27:40 +0100

    Serialize and deserialize binary tree

---
+++ binary_tree.py
@@ -0,0 +1,22 @@
+class Node(object):
+    def __init__(self, value=None):
+        self.parent = None
+        self.left = None
+        self.right = None
+        self.value = value
+    def set_left(self, child):
+        self.left = child
+        child.parent = self
+        return self
+    def set_right(self, child):
+        self.right = child
+        child.parent = self
+        return self
+    def remove_left(self):
+        del self.left
+        self.left = None
+    def remove_right(self):
+        del self.right
+        self.right = None
+    def __eq__(self, other):
+        return self.value == other.value and self.left == other.left and self.right == other.right
---
+++ serialize_binary_tree.py
@@ -0,0 +1,39 @@
+from binary_tree import Node
+
+def serialize(rootnode):
+    if rootnode is None:
+        return ''
+    return f'{rootnode.value}<{serialize(rootnode.left)},{serialize(rootnode.right)}>'
+
+def deserialize(serialized):
+    accumulator = None
+    result = Node()
+    current_node = result
+    for char in serialized:
+        if char.isdigit():
+            if accumulator is None:
+                accumulator = 0
+            accumulator *= 10
+            accumulator += int(char)
+        elif char == '<':
+            current_node.value = accumulator
+            accumulator = None
+            left_node = Node()
+            current_node.set_left(left_node)
+            current_node = left_node
+        elif char == ',':
+            current_node = current_node.parent
+            if current_node.left.value is None:
+                current_node.remove_left()
+            right_node = Node()
+            current_node.set_right(right_node)
+            current_node = right_node
+        elif char == '>':
+            current_node = current_node.parent
+            if current_node.right.value is None:
+                current_node.remove_right()
+    return result
+if __name__ == "__main__":
+    root = Node(5).set_left(Node(4).set_right(Node(8))).set_right(Node(21))
+    d = deserialize(serialize(root))
+    assert d == root
