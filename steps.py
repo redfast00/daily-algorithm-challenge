213c6a189b8a66575f670953e1e4aa8569fb029a
redfast00 <redfast00@gmail.com>
2018-03-09 17:46:04 +0100

    Changed custom list to tuple

--- steps.py
+++ steps.py
@@ -1,12 +1,6 @@
 from functools import lru_cache
 
 
-# Purpose: this class is hashable, and so the recursive_counter function can be cached
-class StepSizes(list):
-    def __hash__(self):
-        return hash(frozenset(self))
-
-
 def count_steps(step_sizes, staircase_length):
     '''Counts the possible ways to go down a staircase with steps of size possible_lengths
     >>> count_steps({1, 2}, 3)
@@ -16,18 +10,18 @@ def count_steps(step_sizes, staircase_length):
     >>> count_steps({5, 6}, 2)
     0
     '''
-    lst = StepSizes(sorted(step_sizes))
+    # convert to tuple so that recursive_counter can be cached
+    lst = tuple(sorted(step_sizes))
     return recursive_counter(lst, staircase_length)
 
 
 @lru_cache(maxsize=2**10)
-def recursive_counter(possible_lengths, staircase_length):
-    print(staircase_length)
+def recursive_counter(step_sizes, staircase_length):
     # expects an ordered possible_lengths
     total_ways = 0
-    for stepsize in possible_lengths:
+    for stepsize in step_sizes:
         if stepsize < staircase_length:
-            total_ways += recursive_counter(possible_lengths, staircase_length - stepsize)
+            total_ways += recursive_counter(step_sizes, staircase_length - stepsize)
         elif stepsize == staircase_length:
             return total_ways + 1
         else:
