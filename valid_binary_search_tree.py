6def47245b9864df4e9ced49bbe05cb1956e3604
redfast00 <redfast00@gmail.com>
2018-05-26 17:45:49 +0200

    Made code easier to read and reason about

--- valid_binary_search_tree.py
+++ valid_binary_search_tree.py
@@ -1,28 +1,22 @@
-def check_valid(root):
+from utils import INFINITY
+
+
+def is_valid_BST(root, lowerbound=-INFINITY, upperbound=INFINITY):
     '''Checks the validity of a binary seach tree given a root node.
     >>> from utils.binary_tree import Node
-    >>> check_valid(Node.from_list([1]))
+    >>> is_valid_BST(Node.from_list([1]))
     True
-    >>> check_valid(Node.from_list([1, [0], [2]]))
+    >>> is_valid_BST(Node.from_list([1, [0], [2]]))
     True
-    >>> check_valid(Node.from_list([1, [3], [2]]))
+    >>> is_valid_BST(Node.from_list([1, [3], [2]]))
     False
     '''
-    valid, _, _ = check_recursive(root)
-    return valid
-
-
-def check_recursive(root):
-    '''Returns if this subtree is valid, the minimum value and the maximum value in it'''
-    this_min = this_max = root.value
-    if root.left is not None:
-        valid, min_left, max_left = check_recursive(root.left)
-        if not valid or max_left > root.value:
-            return False, None, None
-        this_min = min_left
-    if root.right is not None:
-        valid, min_right, max_right = check_recursive(root.right)
-        if not valid or min_right < root.value:
-            return False, None, None
-        this_max = max_right
-    return True, this_min, this_max
+    # Base case
+    if root is None:
+        return True
+    # Check if this node is correct
+    if root.value < lowerbound or root.value > upperbound:
+        return False
+    # Check if children are correct
+    return (is_valid_BST(root.left, lowerbound, root.value) and
+            is_valid_BST(root.right, root.value, upperbound))
